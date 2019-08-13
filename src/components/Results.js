e51e5f587c65ae73cf0c837101c9ae5e52fc7724
Diz.a.t <diz@aily.team>
2019-08-14 00:17:55 +0300

    new search algorithms

--- src/components/Results.js
+++ src/components/Results.js
@@ -2,41 +2,18 @@ import React from 'react'
 import {getSentences} from '../utils/transformStrings'
 import Sentence from './Sentence'
 import {connect} from 'react-redux'
+import {findAll, extractPalindrome} from '../utils/isPalindrome'
 
 const Results = ({ file, palindrome }) => {
   const res = file.content && palindrome.show
     ? getSentences(file.content).map((str, i) => <Sentence content={str} key={i}/>)
     : file.content
 
-  // file.content && findAll(file.content)
-
-  const isPalindromchik = (str) => {
-    if(str.trim().length < 2) {
-      return false
-    }
-
-    const formatted = str.toLowerCase().replace(/[^a-zA-Z]/g, '')
-    const half = Math.floor(formatted.length / 2)
-    const even = formatted.length % 2 === 0
-    let toRight = even ? half + 1 : half
-
-    for (let toLeft = half; toLeft >= 0; toLeft--, toRight++) {
-      if (formatted[toLeft] !== formatted[toRight]) {
-        return false
-      }
-    }
-
-    return true
+  let result = file.content;
+  if (result) {
+    let palindromeList = findAll(res);
+    console.log(palindromeList)
   }
-
-  const nextChar = (text) => {
-    for(let char of text){
-
-      console.log('char: ',char)
-    }
-  }
-
-  file.content && nextChar(file.content)
 
   return (
     <div className="results">
--- src/utils/isPalindrome.js
+++ src/utils/isPalindrome.js
@@ -1,5 +1,5 @@
 export const isPalindrome = (str) => {
-  if(str.trim().length < 2) {
+  if (str.trim().length < 2) {
     return false
   }
 
@@ -15,4 +15,67 @@ export const isPalindrome = (str) => {
   }
 
   return true
+}
+
+const findPalindrome = (text, leftPos, rightPos) => {
+  let palindrome
+
+  const reg = /[^a-zA-Z]/
+
+  let leftChar = text[leftPos]
+  let rightChar = text[rightPos]
+
+  while (leftPos >= 0 && rightPos < text.length) {
+
+    //if chars are not equal && left + right chars not spec symbol
+    if (leftChar !== rightChar && !reg.test(leftChar) && !reg.test(rightChar)) {
+      break
+    }
+
+    //if left char is spec symbol
+    if (reg.test(leftChar)) {
+      leftChar = text[--leftPos]
+      continue
+    }
+
+    //if right char is spec symbol
+    if (reg.test(rightChar)) {
+      rightChar = text[++rightPos]
+      continue
+    }
+
+    palindrome = text.substring(leftPos, rightPos + 1)
+    leftChar = text[--leftPos]
+    rightChar = text[++rightPos]
+  }
+
+  return palindrome
+}
+
+function cutOut (str) {
+  //if is a part of palindrome cut
+  return str
+}
+
+
+export const findAll = (text) => {
+
+  const formattedText = text.toLowerCase().trim()
+
+  let results = []
+  for (let i = 1; i < formattedText.length; i++) {
+    let start = i - 1, end = i + 1
+    let palindrome = findPalindrome(formattedText, start, end)
+
+    if (palindrome)
+      results.push(palindrome)
+
+    start = i - 1
+    end = i
+    palindrome = findPalindrome(formattedText, start, end)
+    if (palindrome)
+      results.push(palindrome)
+
+  }
+  return results
 }
