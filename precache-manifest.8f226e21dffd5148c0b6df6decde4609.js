7148e91fe5a85ba806e073a975d258d8157d7fdc
Diz.a.t <diz@aily.team>
2019-09-14 22:39:06 +0300

    Updates

---
+++ precache-manifest.8f226e21dffd5148c0b6df6decde4609.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b1d4c01dda1abb7daa294b7502e3208",
+    "url": "/palindromes/index.html"
+  },
+  {
+    "revision": "e0936bec9ac264cdc52b",
+    "url": "/palindromes/static/css/main.0d401386.chunk.css"
+  },
+  {
+    "revision": "f1e8050ff0f351332c4f",
+    "url": "/palindromes/static/js/2.ae2147b3.chunk.js"
+  },
+  {
+    "revision": "e0936bec9ac264cdc52b",
+    "url": "/palindromes/static/js/main.2a8e79b8.chunk.js"
+  },
+  {
+    "revision": "3d03cd666e7a532520c5",
+    "url": "/palindromes/static/js/runtime~main.40557287.js"
+  }
+]);
